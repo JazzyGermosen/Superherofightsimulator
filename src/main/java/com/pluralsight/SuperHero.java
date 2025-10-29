df13cbd2ad79696629d6bc8d6a596585d8834aaa
jazzy <jazzygermosen@gmail.com>
2025-10-29 13:11:10 -0400

    fixinf smalls errors withing the super person and hero code

--- src/main/java/com/pluralsight/SuperHero.java
+++ src/main/java/com/pluralsight/SuperHero.java
@@ -5,13 +5,15 @@ import java.util.Random;
 public class SuperHero extends SuperPerson {
     // ^ essentially saying that a superhero is an extention of super person
     //
-    public SuperHero(String name, int health){
+    public SuperHero(String name, int health, String axe, String sword, String lance, String magic){
         // you can use super function to refer to the parents constructor
         Super(name, health);
         // override function is method is used to replace a method from tthe parent class without changing it in its origonal class
         // changing the behavior of the method without actually changing the main method in the parent function
         // this will deal wtih the custom hero stuff
-        this.getExperiencePoints = powerlevel;
+        this.experiencePoints = powerlevel;
+        //abstract methods created in parent class must be used in extended children classes
+        //an abstract method must be overridden in its subclass
     }
 
     @Override
@@ -20,14 +22,15 @@ public class SuperHero extends SuperPerson {
         Random rand = new Random();
         // in this statement we will choose a number between 0 and 101
         // we are adding 101 because the computer will not be reading the 1 in 101
-        int baseDamage = baseDamage + this.experiencePoints;
+        int baseDamage = rand.nextInt(101);
+        int totalDamage = baseDamage + this.experiencePoints;
         //determine if we missed the hit based on the baseDamage
         // if we got 0 for the base damage we missed
         // other wise we landed our attack on the opponent and took damage away from them
         if(baseDamage == 0){
             System.out.println(this.getName() + "HA HA U MISSED LOSER!");
         }else{
-            System.out.println(this.getName() + " hit " + opponent.getName() + " for " + totalDamage + " damage ");
+            System.out.println(this.getName() + " hits " + opponent.getName() + " for " + totalDamage + " damage ");
         }
     }
 }
