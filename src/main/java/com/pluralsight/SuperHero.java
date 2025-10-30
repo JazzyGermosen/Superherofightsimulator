c8e3e877e682f688d99a3c27fb24617d57f60c92
jazzy <jazzygermosen@gmail.com>
2025-10-30 11:37:51 -0400

    fixinf smalls errors withing the super person and hero code

--- src/main/java/com/pluralsight/SuperHero.java
+++ src/main/java/com/pluralsight/SuperHero.java
@@ -5,9 +5,9 @@ import java.util.Random;
 public class SuperHero extends SuperPerson {
     // ^ essentially saying that a superhero is an extention of super person
     //
-    public SuperHero(String name, int health, String axe, String sword, String lance, String magic){
+    public SuperPerson(String name, int health, String axe, String sword, String lance, String magic){
         // you can use super function to refer to the parents constructor
-        Super(name, health);
+        Super(name, health, axe, sword, lance, magic);
         // override function is method is used to replace a method from tthe parent class without changing it in its origonal class
         // changing the behavior of the method without actually changing the main method in the parent function
         // this will deal wtih the custom hero stuff
